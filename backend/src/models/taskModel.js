e707d81d3d2a446f7e6b891295811d3b0baacf81
Javier Renedo <renedogj@gmail.com>
2024-12-12 20:30:54 +0100

    fix tabs

--- backend/src/models/taskModel.js
+++ backend/src/models/taskModel.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 // import { getClient, getdb } from "../db/conection.js";
 
 const taskSchema = new mongoose.Schema({
-    userId: mongoose.Schema.Types.ObjectId,
-    title: String,
-    description: String,
-    status: { type: String, default: "pending" },
-    priority: { type: String, default: "medium" },
-    categoryId: mongoose.Schema.Types.ObjectId,
-    dueDate: Date,
-    sharedWith: [mongoose.Schema.Types.ObjectId],
+	userId: mongoose.Schema.Types.ObjectId,
+	title: String,
+	description: String,
+	status: { type: String, default: "pending" },
+	priority: { type: String, default: "medium" },
+	categoryId: mongoose.Schema.Types.ObjectId,
+	dueDate: Date,
+	sharedWith: [mongoose.Schema.Types.ObjectId],
 }, { timestamps: true });
 
 export default mongoose.model("tasks", taskSchema);
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,46 +4,46 @@ import HomePage from '../views/HomePage.vue'
 import TabBar from '../components/TabBar.vue';
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/homePage'
-  },
-  {
-    path: '/homePage',
-    name: 'HomePage',
-    component: HomePage
-  },
-  {
-    path: '/',
-    component: TabBar,
-    children: [
-      {
-        path: '',
-        redirect: '/home',
-      },
-      {
-        path: 'home',
-        component: () => import('../views/HomePage.vue'),
-      },
-      {
-        path: 'radio',
-        component: () => import('../views/RadioPage.vue'),
-      },
-      {
-        path: 'library',
-        component: () => import('../views/HomePage.vue'),
-      },
-      {
-        path: 'search',
-        component: () => import('../views/HomePage.vue'),
-      },
-    ],
-  },
+	{
+		path: '/',
+		redirect: '/homePage'
+	},
+	{
+		path: '/homePage',
+		name: 'HomePage',
+		component: HomePage
+	},
+	{
+		path: '/',
+		component: TabBar,
+		children: [
+		{
+			path: '',
+			redirect: '/home',
+		},
+		{
+			path: 'home',
+			component: () => import('../views/HomePage.vue'),
+		},
+		{
+			path: 'radio',
+			component: () => import('../views/RadioPage.vue'),
+		},
+		{
+			path: 'library',
+			component: () => import('../views/HomePage.vue'),
+		},
+		{
+			path: 'search',
+			component: () => import('../views/HomePage.vue'),
+		},
+		],
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes
 })
 
 export default router;
